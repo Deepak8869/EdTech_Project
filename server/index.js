65a2953b96835d0db1ccf268dcb9a23642e70642
Deepak8869 <deepak8869880049@gmail.com>
2023-09-12 12:57:33 +0530

    First commit on Edtech Project

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
 
 // Setting up port number
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 // Loading environment variables from .env file
 dotenv.config();
